a7c0446702acef4c7ad1705eb15b12b7520317e8
anders ryndel <anders_ryndel@hotmail.com>
2021-07-15 00:12:28 +0200

    Phase lines

--- RotorheadToABRIS.py
+++ RotorheadToABRIS.py
@@ -1,12 +1,6 @@
-#from urllib.request import urlopen
-#import json
-#import serialize
-#import uuid
 import re
-#import os
-#import sys
 from serverreader import ServerReader
-from geometry import bufferZone, Color, NavPoint
+from geometry import bufferZone, getPhaseLine, Color, NavPoint,Line
 from exportabris import AbrisAdditional, AbrisNavigation
 
 def getSamName(unit):
@@ -14,59 +8,26 @@ def getSamName(unit):
 	name = fullName.split(' ')[0]
 	return name
 def getInfName(unit):
-	c=re.match("Infantry <br>(\d+) Alive",unit.description).groups(1)
-	return c[0]
+	c=re.match("Infantry <br>(\d+) Alive",unit.description).group(1)
+	return c
 
 def getVehcName(unit):
 	name = unit.description.split(' ')[0]
 	return name
 
 def getObjName(unit):
-	obj=re.match("Objective Area: (\w+).+",unit.description).groups(1)
-	return obj[0]
+	obj=re.match("Objective Area: (\w+).+",unit.description).group(1)
+	return obj
 
-#def getPosition(unit):
-#	pos=unit['geometry']['coordinates']
-
-#	return {
-#		"longitude":pos[0],
-#		"latitude":pos[1],
-#		"course":0,
-#		"height":0
-#		}
-#def getZonePoint(pt):
-#	long,lat=pt
-#	return {
-#		"longitude":long,
-#		"latitude":lat
-#		}
-#red={
-#	"R":255,
-#	"G":0,
-#	"B":0}
-
-#def getDangerZones(solutions):
-#	zones=[]
-#	for solution in solutions:
-#		zone = {
-#			"type":1,
-#			"name":"",
-#			"sub_type":0,
-#			"color":red,
-#			"class":"ABRIS_Linear_Object",
-#			"points":[getZonePoint(pt) for pt in solution]
-#			}
-#		zone["points"].append(zone["points"][0])
-#		zones.append(zone)
-#	return zones
-
+def isPhaseLine(unit):
+	return unit.description.startswith('PHASE LINE')
 
 url='http://rotorheads.ddns.net'
 reader=ServerReader(url)
 redForUrl='/mapdata/mapred.json'
 unitsUrl='/mapdata/map.json'
 
-
+# Visible units
 navigation=AbrisNavigation()
 units=reader.readPoints(unitsUrl)
 navPoints = []
@@ -94,88 +55,29 @@ for unit in units:
 	navPoints.append(nav)
 navigation.AddNavPoints(navPoints)
 navigation.Export()
-#units
-#units=json.load(urlopen(unitsUrl));
-#referencePoints= {}
-#for unit in units['features']:
-#	if unit['properties']['description'].startswith('Objective Area'):
-#		type_id=0
-#		name=getObjName(unit)
-#		sub_type=8
-#	elif unit['properties']['icon']=='sam':
-#		type_id=4
-#		name=getSamName(unit)
-#		sub_type=2
-#	elif unit['properties']['icon']=='infant':
-#		type_id=4
-#		sub_type=0
-#		name=getInfName(unit)
-#	elif unit['properties']['icon']=='bvehicle':
-#		type_id=0
-#		sub_type=1
-#		name=getVehcName(unit)
-#	else:
-#		continue
-	
-	
-#	refPoint={
-#		"type":type_id,
-#		"name":name,
-#		"callsign":name,
-#		"class":"ABRIS_Datalink_Point",#"ABRIS_Waypoint",
-#		"position":getPosition(unit),
-#		"sub_type":sub_type,
-#		}
-#	navPoints[uuid.uuid4()]=refPoint
 
-#navigation={
-#	"date":{"year":2016,
-#		"day":22,
-#		"month":6,
-#	},
-#	"expiration_date":{"year":2036,
-#		"day":22,
-#		"month":6,
-#	},
-#	"region":"",
-#	"waypoints":referencePoints
-#    }
-
-#with open(databasePath+'NAVIGATION.lua','w') as file:
-#	data=serialize.dumps(navigation,varname='navigation',indent=0)
-#	file.write(data)
-
-#danger zones
-#
-#buffer=1.5/60
-#scale=1e4
-#dangePoints=json.load(urlopen(dangerZoneUrl));
-#offset=pc.PyclipperOffset()
-#for pt in dangePoints['features']:
-#	pos=(tuple(pt['geometry']['coordinates']),)
-#	offset.AddPath(pc.scale_to_clipper(pos,scale), pc.JT_ROUND, pc.ET_OPENROUND)
-
-#zones = pc.scale_from_clipper(offset.Execute(pc.scale_to_clipper(buffer,scale)),scale)
-
-#additional = {
-#	"date":{"year":2016,
-#		"day":22,
-#		"month":6,
-#	},
-#	"expiration_date":{"year":2036,
-#		"day":22,
-#		"month":6,
-#	},
-#	"additional_objects":getLinesObjects(zones)
-#	}
-#with open(databasePath+'ADDITIONAL.lua','w') as file:
-#	data=serialize.dumps(additional,varname='additional',indent=0)
-#	file.write(data)
-#pass
+# Danger zone
 additionalInfo=AbrisAdditional()
 redFtrs=reader.readPoints(redForUrl)
 redPts=[ftr.location for ftr in redFtrs]
 lines=bufferZone(redPts,Color.red)
 additionalInfo.AddLines(lines)
+
+
+# Phase lines
+phaseLinePoints = {}
+
+for ftr in units: 
+	if isPhaseLine(ftr):
+		name=re.match("PHASE LINE (\w+)",ftr.description).group(1)
+		phaseLinePoints.setdefault(name, []).append(ftr.location)
+
+phaseLines = []
+for name,pts in phaseLinePoints.items():
+	linePts=getPhaseLine(pts)
+	phaseLines.append(Line(linePts,Color.black,"PL "+name))
+
+additionalInfo.AddLines(phaseLines)
+
 additionalInfo.Export()
 pass
--- geometry.py
+++ geometry.py
@@ -1,5 +1,6 @@
 import pyclipper as pc
 from typing import List,Tuple
+from itertools import permutations
 
 scale=1e4
 bufferSize=1.5#Nm
@@ -20,13 +21,57 @@ def bufferZone(points:List[Tuple[float,float]], color):
 		lines.append(Line(points,color))
 	return lines
 
+def distanceSqr(pt0,pt1)->float:
+	x=pt0[0]-pt1[0]
+	y=pt0[1]-pt1[1]
+	return x*x+y*y
+
+def getPhaseLine(phaseLinePts):
+	count=len(phaseLinePts)
+	paths=[]
+	# ugly n! brute force algorithm, good enough if there's only 3 points
+	for combo in permutations(range(0,count)):
+		dist=0
+		for i, j in zip(combo[:-1],combo[1:]):
+			dist=dist+distanceSqr(phaseLinePts[i],phaseLinePts[j])
+		paths.append((dist,combo))
+	bestPath = min(paths)[1]
+	return [phaseLinePts[i] for i in bestPath]
+	
+
+	#segments = {}
+	#for item in table:
+	#	segment1 = segments.pop(item[1],None)
+	#	segment2 = segments.pop(item[2],None)
+	#	if segment1 and segment2:
+	#		segment1.extend(segment2)
+	#	elif segment1 and not segment2:
+	#		segment1.append(item[2])
+	#	elif segment2 and not segment1:
+	#		segment1=segment2
+	#		segment1.append(item[2])
+
+	#	else:
+	#		segments[item[1]]=[item[1],item[2]]
+	#		segments[item[2]]=[item[2],item[1]]
+	# segments[0]]
+
 class Color:
     red={"R":255,
 	     "G":0,
 	     "B":0}
-    yellow={"R":255,
-	     "G":255,
+    yellow={"R":190,
+	     "G":128,
 	     "B":0}
+    green={"R":0,
+	       "G":255,
+	       "B":0}
+    white={"R":255,
+	       "G":255,
+	       "B":255}
+    black={"R":0,
+	       "G":0,
+	       "B":0}
 
 class Line:
     name=""
