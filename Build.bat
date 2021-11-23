7f04dd3a42d52f37ace1ab90c4d4fc69cd0ee20b
anders ryndel <anders_ryndel@hotmail.com>
2021-11-23 23:00:16 +0100

    export to abris

--- Build.bat
+++ Build.bat
@@ -1,10 +1,10 @@
-"./env/scripts/pyinstaller" -F ./ExportToABRIS.py
-del /S /Q "./dist/ExportToABRIS/"
-del /S /Q "./dist/ExportToABRIS.zip"
-if not exist "./dist/ExportToABRIS/" mkdir "./dist/ExportToABRIS/"
-copy "./disableCategories.json" "./dist/ExportToABRIS/disableCategories.json"
-copy "./Specs/*" "./dist/ExportToABRIS/"
+.\env\scripts\pyinstaller -F .\ExportToABRIS.py
+del /S /Q ".\dist\ExportToABRIS\"
+del /S /Q ".\dist\ExportToABRIS.zip"
+if not exist ".\dist\ExportToABRIS\" mkdir ".\dist\ExportToABRIS\"
+copy ".\disableCategories.json" ".\dist\ExportToABRIS\disableCategories.json"
+copy ".\Specs\*" ".\dist\ExportToABRIS\"
 cd dist
-copy "./ExportToABRIS.exe" "./ExportToABRIS/ExportToABRIS.exe"
-zip ExportToABRIS.zip ExportToABRIS/*
+copy ".\ExportToABRIS.exe" ".\ExportToABRIS\ExportToABRIS.exe"
+zip ExportToABRIS.zip ExportToABRIS\*
 cd ..
